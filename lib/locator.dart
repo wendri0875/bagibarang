3034907a9523e0e415413bf3ad963f8d148e936d
Muntarko Wendri C. (Unit 1 - EDP) <wendri@lmserver.local>
2020-04-27 15:45:00 +0700

    storage

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,7 +1,9 @@
 import 'package:bagi_barang/services/FirestoreService.dart';
 import 'package:bagi_barang/services/authentication_service.dart';
+import 'package:bagi_barang/services/cloud_storage_service.dart';
 import 'package:bagi_barang/services/dialog_service.dart';
 import 'package:bagi_barang/services/navigation_service.dart';
+import 'package:bagi_barang/utils/image_selector.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
@@ -11,4 +13,6 @@ void setupLocator() {
   locator.registerLazySingleton(() => DialogService());
   locator.registerLazySingleton(() => AuthenticationService());
   locator.registerLazySingleton(() => FirestoreService());
+  locator.registerLazySingleton(() => CloudStorageService());
+  locator.registerLazySingleton(() => ImageSelector());
 }
---
+++ lib/services/cloud_storage_service.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+class CloudStorageService {
+  Future<CloudStorageResult> uploadImage({
+    @required File imageToUpload,
+    @required String title,
+  }) async {
+ var imageFileName = title + DateTime.now().millisecondsSinceEpoch.toString();
+
+    final StorageReference firebaseStorageRef = FirebaseStorage.instance
+    .ref()
+    .child(imageFileName);
+
+    StorageUploadTask uploadTask = firebaseStorageRef.putFile(imageToUpload);
+
+    StorageTaskSnapshot storageSnapshot = await uploadTask.onComplete;
+
+    var downloadUrl = await storageSnapshot.ref.getDownloadURL();
+
+    if (uploadTask.isComplete) {
+      var url = downloadUrl.toString();
+      return CloudStorageResult(
+        imageUrl: url,
+        imageFileName: imageFileName,
+        );
+    }
+
+    return null;
+  }
+}
+
+class CloudStorageResult {
+  final String imageUrl;
+  final String imageFileName;
+
+  CloudStorageResult({this.imageUrl, this.imageFileName});
+}
